696a61b49e1084571d3997166c9b9ac089159ca4
sanyam810 <sanyamsaini081@gmail.com>
2024-01-23 06:10:35 +0530

    implemented flags (brainCells lost)

--- app/(singlePageBlog)/posts/[slug]/page.jsx
+++ app/(singlePageBlog)/posts/[slug]/page.jsx
@@ -29,7 +29,7 @@ const descStyle2 = {
   // Add more font styles as needed
 };
 
-const getData = async (slug, setLikesCount) => {
+const getData = async (slug, setLikesCount,setGreenCount,setRedCount,setYellowCount) => {
   const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
     cache: "no-store",
   });
@@ -40,6 +40,9 @@ const getData = async (slug, setLikesCount) => {
     if (res.ok) {
       data = await res.json();
       setLikesCount(data.likesCount);
+      setGreenCount(data.greenCount);
+      setRedCount(data.redCount);
+      setYellowCount(data.yellowCount);
     } else {
       throw new Error("Failed");
     }
@@ -54,34 +57,86 @@ const SinglePage = ({ params }) => {
   const { slug } = params;
   const [likesCount, setLikesCount] = useState(0);
   const [liked, setLiked] = useState(false);
+  const [greenCount, setGreenCount] = useState(0);
+  const [green, setGreen] = useState(false);
+  const [yellowCount, setYellowCount] = useState(0);
+  const [yellow, setYellow] = useState(false);
+  const [redCount, setRedCount] = useState(0);
+  const [red, setRed] = useState(false);
   const [data, setData] = useState(null); // State to store the fetched data
   const { data: session, status } = useSession();
+  
 
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await getData(slug, setLikesCount);
+    try {
+      const result = await getData(slug, setLikesCount, setGreenCount, setRedCount, setYellowCount);
       setData(result);
-    };
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
     const checkLikeStatus = async () => {
-      // Check if the user is signed in
       if (status === 'authenticated') {
-        // Use session.user.email or other user properties as needed
         const email = session?.user?.email;
-    
-        // Check if the cookie exists for the liked status
-        const likedStatus = Cookies.get(`liked_post_${slug}_${email}`);
+        const likedStatus = Cookies.get(`liked_post_${slug}_${email}_like`);
         if (likedStatus === 'true') {
           setLiked(true);
         } else {
-          setLiked(false); // Set to false if the cookie is not present
+          setLiked(false);
         }
       }
     };
 
-    fetchData(); // Invoke the async function immediately
+    const checkGreenStatus = async () => {
+      if (status === 'authenticated') {
+        const email = session?.user?.email;
+        const greenStatus = Cookies.get(`liked_post_${slug}_${email}_green`);
+        if (greenStatus === 'true') {
+          setGreen(true);
+        } else {
+          setGreen(false);
+        }
+      }
+    };
+
+
+    const checkYellowStatus = async() => {
+      if (status === 'authenticated') {
+        const email = session?.user?.email;
+        const yellowStatus = Cookies.get(`liked_post_${slug}_${email}_yellow`);
+        if (yellowStatus === 'true') {
+          setYellow(true);
+        } else {
+          setYellow(false);
+        }
+      }
+    };
+
+    const checkRedStatus = async() => {
+      if (status === 'authenticated') {
+        const email = session?.user?.email;
+        const redStatus = Cookies.get(`liked_post_${slug}_${email}_red`);
+        if (redStatus === 'true') {
+          setRed(true);
+        } else {
+          setRed(false);
+        }
+      }
+    };
+
+
+
+
+    // Invoke the async function immediately
     checkLikeStatus();
+    checkGreenStatus();
+    checkYellowStatus();
+    checkRedStatus();
+
+    fetchData(); 
 
   }, [slug,session, status]);
 
@@ -104,7 +159,7 @@ const SinglePage = ({ params }) => {
 
           if (res.ok) {
             // Remove the cookie and update state
-            Cookies.remove(`liked_post_${slug}_${email}`);
+            Cookies.remove(`liked_post_${slug}_${email}_like`);
             setLiked(false);
             setLikesCount((prevCount) => prevCount - 1);
           } else {
@@ -122,9 +177,165 @@ const SinglePage = ({ params }) => {
 
           if (res.ok) {
             // Set the cookie and update state
-            Cookies.set(`liked_post_${slug}_${email}`, 'true', { expires: 30 });
+            Cookies.set(`liked_post_${slug}_${email}_like`, 'true', { expires: 30 });
             setLiked(true);
             setLikesCount((prevCount) => prevCount + 1);
+          } else {
+            console.error('Failed to like the post');
+          }
+        }
+      } else {
+        console.error('User not signed in');
+      }
+    } catch (error) {
+      console.error('Error liking/unliking post:', error);
+    }
+  };
+
+  const handleGreen = async () => {
+    try {
+      // Check if the user is signed in
+      if (status === 'authenticated') {
+        const email = session?.user?.email;
+
+        // Check if the post is already liked
+        if (green) {
+          // Unlike the post
+          const res = await fetch(`http://localhost:3000/api/posts/${slug}/greenflagul`, {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ slug }),
+          });
+
+          if (res.ok) {
+            // Remove the cookie and update state
+            Cookies.remove(`liked_post_${slug}_${email}_green`);
+            setGreen(false);
+            setGreenCount((prevCount) => prevCount - 1);
+          } else {
+            console.error('Failed to unlike the post');
+          }
+        } else {
+          // Like the post
+          const res = await fetch(`http://localhost:3000/api/posts/${slug}/greenflagl`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ slug }),
+          });
+
+          if (res.ok) {
+            // Set the cookie and update state
+            Cookies.set(`liked_post_${slug}_${email}_green`, 'true', { expires: 30 });
+            setGreen(true);
+            setGreenCount((prevCount) => prevCount + 1);
+          } else {
+            console.error('Failed to like the post');
+          }
+        }
+      } else {
+        console.error('User not signed in');
+      }
+    } catch (error) {
+      console.error('Error liking/unliking post:', error);
+    }
+  };
+
+  const handleYellow = async () => {
+    try {
+      // Check if the user is signed in
+      if (status === 'authenticated') {
+        const email = session?.user?.email;
+
+        // Check if the post is already liked
+        if (yellow) {
+          // Unlike the post
+          const res = await fetch(`http://localhost:3000/api/posts/${slug}/yellowflagul`, {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ slug }),
+          });
+
+          if (res.ok) {
+            // Remove the cookie and update state
+            Cookies.remove(`liked_post_${slug}_${email}_yellow`);
+            setYellow(false);
+            setYellowCount((prevCount) => prevCount - 1);
+          } else {
+            console.error('Failed to unlike the post');
+          }
+        } else {
+          // Like the post
+          const res = await fetch(`http://localhost:3000/api/posts/${slug}/yellowflagl`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ slug }),
+          });
+
+          if (res.ok) {
+            // Set the cookie and update state
+            Cookies.set(`liked_post_${slug}_${email}_yellow`, 'true', { expires: 30 });
+            setYellow(true);
+            setYellowCount((prevCount) => prevCount + 1);
+          } else {
+            console.error('Failed to like the post');
+          }
+        }
+      } else {
+        console.error('User not signed in');
+      }
+    } catch (error) {
+      console.error('Error liking/unliking post:', error);
+    }
+  };
+
+  const handleRed = async () => {
+    try {
+      // Check if the user is signed in
+      if (status === 'authenticated') {
+        const email = session?.user?.email;
+
+        // Check if the post is already liked
+        if (red) {
+          // Unlike the post
+          const res = await fetch(`http://localhost:3000/api/posts/${slug}/redflagul`, {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ slug }),
+          });
+
+          if (res.ok) {
+            // Remove the cookie and update state
+            Cookies.remove(`liked_post_${slug}_${email}_red`);
+            setRed(false);
+            setRedCount((prevCount) => prevCount - 1);
+          } else {
+            console.error('Failed to unlike the post');
+          }
+        } else {
+          // Like the post
+          const res = await fetch(`http://localhost:3000/api/posts/${slug}/redflagl`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ slug }),
+          });
+
+          if (res.ok) {
+            // Set the cookie and update state
+            Cookies.set(`liked_post_${slug}_${email}_red`, 'true', { expires: 30 });
+            setRed(true);
+            setRedCount((prevCount) => prevCount + 1);
           } else {
             console.error('Failed to like the post');
           }
@@ -185,6 +396,45 @@ const SinglePage = ({ params }) => {
             )}
             </div>
             <span>{likesCount} likes</span>
+          </div>
+
+          <div>
+            <div>
+            {session && (
+            <div>
+              <button onClick={handleGreen} style={{ background: green ? 'green' : 'none' }}>
+                {green ? 'Unmark' : 'Mark Green'}
+              </button>
+            </div>
+            )}
+            </div>
+            <span>{greenCount} likes</span>
+          </div>
+
+          <div>
+            <div>
+            {session && (
+            <div>
+              <button onClick={handleYellow} style={{ background: yellow ? 'yellow' : 'none' }}>
+                {yellow ? 'Unmark' : 'Mark Yellow'}
+              </button>
+            </div>
+            )}
+            </div>
+            <span>{yellowCount} likes</span>
+          </div>
+
+          <div>
+            <div>
+            {session && (
+            <div>
+              <button onClick={handleRed} style={{ background: red ? 'red' : 'none' }}>
+                {red ? 'Unmark' : 'Mark Red'}
+              </button>
+            </div>
+            )}
+            </div>
+            <span>{redCount} likes</span>
           </div>
 
           <div style={{ marginTop: '2rem', marginBottom: '1rem' }}>
